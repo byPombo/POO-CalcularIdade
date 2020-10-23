bdda8200596ae988018c3a2650f23eff2d8ac609
Daniel Victor <daniel.silva@apinteractive.com.br>
2020-10-23 11:56:31 -0300

    Calcula a idade pelo ano de nascimento

---
+++ CalculaIdade/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a Idade de uma pessoa");
+            Pessoa p = new Pessoa();
+            Console.Write("Nome da pessoa: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Ano de nascimento: ");
+            p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            p.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
